81d763b1fa9e3812a6e94f8ae5baca828f4c60e9
Irakli <i.botchori@gmail.com>
2022-07-04 14:22:20 +0400

    feat: add user model

---
+++ src/models/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Please add a email'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password'],
+    },
+  },
+  // timestamps option that tells Mongoose to automatically manage createdAt and updatedAt properties on your documents.
+  { timestamps: true }
+)
+const User = mongoose.model('User', userSchema)
+
+export default User
