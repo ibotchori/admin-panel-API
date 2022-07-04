40874e7a66ad487d4cdb3e3557e8ecf57211106d
Irakli <i.botchori@gmail.com>
2022-07-04 10:05:40 +0400

    feat: add express async handler

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,19 @@
+import asyncHandler from 'express-async-handler'
+
 // @desc Register new user
 // @route POST /register
 // @access Public
-export const register = (req, res) => {
+export const register = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400)
     throw new Error('Please add a text')
   }
   res.status(200).json(req.body)
-}
+})
 
 // @desc Authenticate a user
 // @route POST /login
 // @access Public
-export const login = (req, res) => {
+export const login = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Login' })
-}
+})
