bc9c9a44e7901c0a589aba2867436f31108687bc
Irakli <i.botchori@gmail.com>
2022-07-04 15:42:51 +0400

    feat: add user to database (register user)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,14 +1,48 @@
 import asyncHandler from 'express-async-handler'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel'
 
 // @desc Register new user
 // @route POST /register
 // @access Public
 export const register = asyncHandler(async (req, res) => {
-  if (!req.body.text) {
+  // Extract name, email and password from request body
+  const { email, password } = req.body
+  // if one of them is not in request body, trow error
+  if (!email || !password) {
     res.status(400)
-    throw new Error('Please add a text')
+    throw new Error('Please add all fields')
   }
-  res.status(200).json(req.body)
+
+  // Check if user exist
+  const userExist = await User.findOne({ email })
+  if (userExist) {
+    res.status(400)
+    throw new Error('User already exist')
+  }
+
+  // Hash password
+  // create salt
+  const salt = await bcrypt.genSalt(10)
+  // generate hashed password with salt (password = entered password, from request body)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // Create User
+  const user = await User.create({
+    email,
+    password: hashedPassword,
+  })
+  // back user information on response
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      email: user.email,
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc Authenticate a user
