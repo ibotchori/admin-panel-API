947ce26e72be90400e8c3108948a12c1f778b6b5
Irakli <i.botchori@gmail.com>
2022-07-25 18:27:51 +0400

    fix: regex on company's url field

--- src/schemas/companyRegistrationSchema.js
+++ src/schemas/companyRegistrationSchema.js
@@ -32,7 +32,9 @@ const companyRegistrationSchema = async (data) => {
       }),
 
     url: Joi.string()
-      .regex(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
+      .regex(
+        /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+      )
       .required()
       .messages({
         'string.pattern.base': `URL format is required.`,
